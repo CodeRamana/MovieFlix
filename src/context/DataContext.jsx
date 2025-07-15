5b6a6dcfd5cf3c3729dbe16e146ced8b5ce226cf
CodeRamana <mailgo2sr@gmail.com>
2025-07-15 09:51:00 +0530

    DataContext is modified

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,23 +1,35 @@
-import {createContext, useContext, useReducer, useState } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
 import searchReducer from "../reducers/searchReducer";
 import pageNavReducer from "../reducers/pageNavReducer";
 
 const DataContext = createContext({});
 
-export const DataProvider = ({children}) => {
-    const [movies,setMovies] = useState([]);
-    const [search , dispatch] = useReducer(searchReducer,{search:"",category:""});
-    const [page,pageDispatch] = useReducer(pageNavReducer,{currentPage:1,totalPage:1})
-    const [details,setDetails] = useState({})
-    return(
-        <DataContext.Provider value={{movies,setMovies,search,dispatch,details,setDetails,page,pageDispatch}}>
-            {children}
-        </DataContext.Provider>
-    );
-}
+export const DataProvider = ({ children }) => {
+  const [movies, setMovies] = useState([]);
+  const [search, dispatch] = useReducer(searchReducer, { search: "", category: "" });
+  const [page, pageDispatch] = useReducer(pageNavReducer, { currentPage: 1, totalPage: 1 });
+  const [details, setDetails] = useState({});
 
-const useDataContext = () =>{
-    return useContext(DataContext);
-}
+  return (
+    <DataContext.Provider
+      value={{
+        movies,
+        setMovies,
+        search,
+        dispatch,
+        details,
+        setDetails,
+        page,
+        pageDispatch,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+const useDataContext = () => {
+  return useContext(DataContext);
+};
 
 export default useDataContext;
